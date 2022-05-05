f33be28c745e72895f2e9bdaeaedc29deb5fe046
CarcamoColoaneValeriaRocio <104532760+CarcamoColoaneValeriaRocio@users.noreply.github.com>
2022-05-05 17:27:24 -0300

    instrucciones2022 trabajo

---
+++ src/ejercicio1.ts
@@ -0,0 +1,22 @@
+console.log("Precalenta el horno a 176 grados");
+console.log("pon grasa y harina en un molde");
+console.log("colocar harina, polvo de hornear y sal");
+console.log("revolver todo para que se mezcle");
+console.log(
+  "colocar azucar, la manteca, los huevos, la leche y la vainilla en un recipiente separado"
+);
+console.log(
+  "con el primer recipiente con los productos secos y mezclalos con la ultima masa hecha"
+);
+console.log(
+  "mezclar a velocidad media unos 2 o 3 minutos hasta que quede homogenea"
+);
+console.log(
+  "una vez tenemos la mezcla, colocala en la cacerola reparada para hornear durante 25-30 minutos"
+);
+console.log(
+  "Para saber si la torta esta lista, utiliza un palillo y ubicalo en el centro de la torta se sale limpio ya tienes la torta hecha"
+);
+console.log(
+  "Antes de poner cualquier tipo de decoracion es importante dejarla enfriar"
+);
